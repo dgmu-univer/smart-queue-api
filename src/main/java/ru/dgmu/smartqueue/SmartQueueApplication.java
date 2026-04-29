d4e82c5bdefee2fe6bc41fa1ee441926cd58d03d
rusgad <rusgad1977@gmail.com>
2026-04-29 12:51:21 +0300

    Init commit

---
+++ src/main/java/ru/dgmu/smartqueue/SmartQueueApplication.java
@@ -0,0 +1,13 @@
+package ru.dgmu.smartqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQueueApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartQueueApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/dgmu/smartqueue/SmartQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dgmu.smartqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
