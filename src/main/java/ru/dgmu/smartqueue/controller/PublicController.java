c301b66ffed6d980507d266975f21de1b728423c
rusgad <rusgad1977@gmail.com>
2026-05-06 10:55:06 +0300

    add public endpoint with mock json

---
+++ src/main/java/ru/dgmu/smartqueue/controller/PublicController.java
@@ -0,0 +1,21 @@
+package ru.dgmu.smartqueue.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dgmu.smartqueue.dto.MockJsonDto;
+
+@RestController
+@Slf4j
+@RequestMapping("/public")
+public class PublicController {
+
+  @GetMapping("/mock")
+  public ResponseEntity<MockJsonDto> getMockJson() {
+    return ResponseEntity.ok(new MockJsonDto("name", "description"));
+  }
+
+
+}
---
+++ src/main/java/ru/dgmu/smartqueue/dto/MockJsonDto.java
@@ -0,0 +1,8 @@
+package ru.dgmu.smartqueue.dto;
+
+public record MockJsonDto(
+    String name,
+    String description
+) {
+
+}
