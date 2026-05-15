bdaac83d863c7cfe29348ff0403cc9ffad8cf167
rusgad <rusgad1977@gmail.com>
2026-05-15 12:06:35 +0300

    Fixed cors

--- src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
+++ src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig {
           corsConfiguration.setAllowedOriginPatterns(List.of("*"));
           corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
           corsConfiguration.setAllowedHeaders(List.of("*"));
-          corsConfiguration.setAllowCredentials(true);
           return corsConfiguration;
         }))
         .authenticationProvider(authenticationProvider())
