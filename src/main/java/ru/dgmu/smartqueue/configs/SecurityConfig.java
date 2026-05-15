16e46fe75709484e95e25184af0b957f6cfe6ddf
rusgad <rusgad1977@gmail.com>
2026-05-15 12:13:44 +0300

    Fixed cors

--- src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
+++ src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,7 +41,7 @@ public class SecurityConfig {
         )
         .cors(cors -> cors.configurationSource(request -> {
           var corsConfiguration = new CorsConfiguration();
-          corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+          corsConfiguration.setAllowedOriginPatterns(List.of("https://price05.ru")); // todo вынести в переменную
           corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
           corsConfiguration.setAllowedHeaders(List.of("*"));
           return corsConfiguration;
