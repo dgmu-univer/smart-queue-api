fc033f791ee4274f2c9b883cbf1572dacce5401d
Мирзалиев Арсен <mirzalievag@akbarsdigital.ru>
2026-06-06 12:59:31 +0300

    Switch deployment branch to release and update CORS origins

--- src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
+++ src/main/java/ru/dgmu/smartqueue/configs/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         .cors(cors -> cors.configurationSource(request -> {
           var corsConfiguration = new CorsConfiguration();
           corsConfiguration.setAllowedOriginPatterns(
-              List.of("https://price05.ru", "http://localhost:3000")); // todo вынести в переменную
+              List.of("http://80.78.247.237", "http://localhost:3000")); // todo вынести в переменную
           corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
           corsConfiguration.setAllowedHeaders(List.of("*"));
           return corsConfiguration;
