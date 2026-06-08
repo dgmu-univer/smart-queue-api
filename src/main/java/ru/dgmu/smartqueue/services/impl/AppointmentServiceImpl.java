45a10ef067ed35ddd2e9ef71fafbb0668d233ae3
rusgad <rusgad1977@gmail.com>
2026-06-08 18:56:44 +0300

    test deploy

--- src/main/java/ru/dgmu/smartqueue/services/impl/AppointmentServiceImpl.java
+++ src/main/java/ru/dgmu/smartqueue/services/impl/AppointmentServiceImpl.java
@@ -93,7 +93,7 @@ public class AppointmentServiceImpl implements AppointmentService {
       throw new IncorrectVerificationCode("Неверный код верификации");
     }
     appointmentRepository.deleteById(verificationRequest.id());
-    return AppointmentDto.fromEntity(appointmentRepository.saveAndFlush(appointment));
+    return AppointmentDto.fromEntity(appointmentRepository.save(appointment));
   }
 
   @Override
