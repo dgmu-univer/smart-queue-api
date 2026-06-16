72209523cfa28f72b19171adc670c77ecb601f73
rusgad <rusgad1977@gmail.com>
2026-06-16 15:01:38 +0300

    Add sms sending via alpha-name rules

--- src/main/java/ru/dgmu/smartqueue/services/impl/MobileVerificationSenderServiceImpl.java
+++ src/main/java/ru/dgmu/smartqueue/services/impl/MobileVerificationSenderServiceImpl.java
@@ -21,10 +21,21 @@ public class MobileVerificationSenderServiceImpl implements MobileVerificationSe
   public void sendVerificationCode(String phone, String verificationCode) {
     try {
       var parsedPhone = phoneUtil.parse(phone, "RU");
-      var phoneWithCountryCode = parsedPhone.getCountryCode() + "" + parsedPhone.getNationalNumber();
-      smsClient.sendSms(SMS_TEMPLATE.formatted(verificationCode), phoneWithCountryCode);
+      var phoneWithCountryCode =
+          parsedPhone.getCountryCode() + "" + parsedPhone.getNationalNumber();
+      smsClient.sendSms(SMS_TEMPLATE.formatted(verificationCode), phoneWithCountryCode,
+          isMtsNumber(phone));
     } catch (NumberParseException e) {
       throw new SmsSendingException(e);
     }
+  }
+
+  public boolean isMtsNumber(String phone) {
+    if (phone == null || phone.length() != 10) {
+      return false;
+    }
+    int code = (phone.charAt(0) - '0') * 100 + (phone.charAt(1) - '0') * 10 + (phone.charAt(2) - '0');
+    return (code >= 910 && code <= 919) || (code >= 980 && code <= 989) ||
+        code == 958 || code == 978 || code == 861 || code == 862;
   }
 }
